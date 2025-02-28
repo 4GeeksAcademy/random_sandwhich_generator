14e5b2247dda360d168f160a64e5f1565e7602bd
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2025-02-28 23:36:28 +0000

    create your sandwhich today

--- src/app.js
+++ src/app.js
@@ -6,26 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  generateSandwhich()
+  document.querySelector("#sandwhichBtn").addEventListener("click", generateSandwhich)
 };
 const generateSandwhich = () => {
+  const jellyDiv = document.querySelector(".jelly")
   let bread = ["White", "Wheat", "Rye", "Whole Grain", "Oat"];
   let butter = ["Peanut Butter", "Almond Butter", "Nutella", "Cashew Butter", "Garlic Butter"];
   let jelly = ["Grape", "Strawberry", "Apple", "Mixed Berry", "Honey"];
 
+  let randomBread = bread[Math.floor(Math.random() * bread.length)]
+  let randomButter = butter[Math.floor(Math.random() * butter.length)];
+  let randomJelly = jelly[Math.floor(Math.random() * jelly.length)];
+
+  if(randomJelly == 'Grape') {
+    jellyDiv.style.color = "purple"
+  }
+  else if(randomJelly == 'Strawberry'){
+    jellyDiv.style.color = "pink"
+  }
+  else if(randomJelly == 'Apple'){
+    jellyDiv.style.color = "red"
+  }
+  else if(randomJelly == 'Mixed Berry'){
+    jellyDiv.style.color = "maroon"
+  }
+  else if(randomJelly == 'Honey'){
+    jellyDiv.style.color = "gold"
+  }
+  else {
+    jellyDiv.style.color = "black"
+  }
   //               round down. random # 0-1     * decimal by length of array to get whole number
-  let breadIndex = Math.floor(Math.random() * bread.length)
-  let butterIndex = Math.floor(Math.random() * butter.length)
-  let jellyIndex = Math.floor(Math.random() * jelly.length)
-
-  //                getting the item of an array at the random index
-  let randomBread = bread[breadIndex];
-  let randomButter = butter[butterIndex];
-  let randomJelly = jelly[jellyIndex];
-
-  // let get the html elments and add these items to it
   document.querySelector(".top").innerHTML = randomBread;
   document.querySelector(".butter").innerHTML = randomButter;
-  document.querySelector(".jelly").innerHTML = randomJelly;
+  jellyDiv.innerHTML = randomJelly;
   document.querySelector(".bottom").innerHTML = randomBread;
 }
